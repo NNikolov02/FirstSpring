84d6accf138bde0579c5a7982092a37644db0344
NNikolov02 <nikinikolov2002@gmail.com>
2023-04-09 18:18:50 +0300

    First Spring

--- src/main/java/mapper/PersonMapper.java
+++ src/main/java/mapper/PersonMapper.java
@@ -4,13 +4,15 @@ import com.firstspring.firstspring.model.Person;
 import dto.PersonCreateRequest;
 import dto.PersonResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 
 @Mapper(uses = AddressMapper.class)
 public interface PersonMapper {
 
-
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "photos", ignore = true)
     Person modelFromCreateRequest(PersonCreateRequest personCreateDto);
 
     PersonResponse responseFromModel(Person person);
